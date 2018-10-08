a57c035554ece83b1a498d4872dfc6968f507466
Izudin Vragic <izudinvragic@Izudins-MacBook-Pro-2.local>
2018-10-08 16:49:01 +0200

    Fixed issues with pullToReload and using values directly from realm

---
+++ Push/Category.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+Category.h
+//  Push
+//
+//  Created by Izudin Vragic on 10/09/2018.
+//  Copyright © 2018 OCCRP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Category)
+
+@end
---
+++ Push/Category.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+Category.m
+//  Push
+//
+//  Created by Izudin Vragic on 10/09/2018.
+//  Copyright © 2018 OCCRP. All rights reserved.
+//
+
+#import "NSObject+Category.h"
+
+@implementation NSObject (Category)
+
+@end
